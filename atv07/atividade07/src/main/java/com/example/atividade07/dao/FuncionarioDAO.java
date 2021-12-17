c6278771b105d03b220cb6b6e82654feaad2acb3
pianks <vinnicius@alu.ufc.br>
2021-12-17 11:33:19 -0300

    atv 07

---
+++ atv07/atividade07/src/main/java/com/example/atividade07/dao/FuncionarioDAO.java
@@ -0,0 +1,19 @@
+package com.example.atividade07.dao;
+import java.util.List;
+
+import com.example.atividade07.entity.Funcionario;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+@Repository
+public interface FuncionarioDAO extends JpaRepository<Funcionario, Integer> {
+
+    public Funcionario findFirstByCpf(String cpf);
+
+    @Query("select f from Funcionario f where f.id = :id")
+    public Funcionario findFistByid(int id);
+
+    public List<Funcionario> findByNomeStartingWith(String str);
+
+}
